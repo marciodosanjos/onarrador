8e84ea2538345a410eb9f81389c6bdd9fdaad7d5
Marcio dos Anjos <63827203+marciodosanjos@users.noreply.github.com>
2023-10-07 11:48:15 +0000

    Update layout.tsx and index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,27 @@
-"use client"; // This is a client component 👈🏽
-
 import Box from "./../components/Box";
-import React, { useState, useEffect } from "react";
+import Layout from "./../components/layout";
 
-interface Items {
-  title: string;
-  description: string;
-}
-
-export default function Home() {
-  const [data, setData] = useState<Items[]>([]);
-
-  useEffect(() => {
-    fetch("https://onarrador.com/wp-json/wp/v2/home-service-item")
-      .then((response) => response.json()) // Parse response as JSON
-      .then((postData: any) => {
-        let objects = postData.map((el: any) => ({
-          title: el.title.rendered,
-          description: el.content.rendered,
-        }));
-
-        setData(objects);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar dados da landing page:", error);
-      });
-  }, []);
-
+export default function Home({ data }) {
+  console.log(data);
   return (
     <div>
-      {data.map((el, ind) => (
-        <Box key={ind} title={el.title} description={el.description} />
+      {data?.map((el) => (
+        <Box title={el.title.rendered} description={el.content.rendered} />
       ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://onarrador.com/wp-json/wp/v2/home-service-item",
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10,
+  };
 }
